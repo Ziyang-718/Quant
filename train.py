869636e9535de5bdf746bc8d89c80b2d8ea8325f
Jason Zheng <jasonz4200@gmail.com>
2025-04-19 03:10:14 -0400

    add

--- train.py
+++ train.py
@@ -2,24 +2,24 @@
 # 词级别的中文RoFormer预训练
 # MLM任务
 
+import jieba
+from bert4keras.snippets import text_segmentate
+from bert4keras.snippets import DataGenerator
+from bert4keras.snippets import sequence_padding, open
+from bert4keras.optimizers import extend_with_gradient_accumulation
+from bert4keras.optimizers import extend_with_piecewise_linear_lr
+from bert4keras.optimizers import extend_with_weight_decay
+from bert4keras.optimizers import Adam
+from bert4keras.tokenizers import Tokenizer
+from bert4keras.models import build_transformer_model
+from bert4keras.layers import Loss
+from bert4keras.backend import keras, K
+import tensorflow as tf
+import numpy as np
+import json
 import os
 os.environ['TF_KERAS'] = '1'  # 必须使用tf.keras
 
-import json
-import numpy as np
-import tensorflow as tf
-from bert4keras.backend import keras, K
-from bert4keras.layers import Loss
-from bert4keras.models import build_transformer_model
-from bert4keras.tokenizers import Tokenizer
-from bert4keras.optimizers import Adam
-from bert4keras.optimizers import extend_with_weight_decay
-from bert4keras.optimizers import extend_with_piecewise_linear_lr
-from bert4keras.optimizers import extend_with_gradient_accumulation
-from bert4keras.snippets import sequence_padding, open
-from bert4keras.snippets import DataGenerator
-from bert4keras.snippets import text_segmentate
-import jieba
 jieba.initialize()
 
 # 基本参数
@@ -28,16 +28,16 @@ batch_size = 64
 epochs = 100000
 
 # bert配置
-config_path = '/root/kg/bert/chinese_wobert_plus_L-12_H-768_A-12/bert_config.json'
-checkpoint_path = '/root/kg/bert/chinese_wobert_plus_L-12_H-768_A-12/bert_model.ckpt'
-dict_path = '/root/kg/bert/chinese_wobert_plus_L-12_H-768_A-12/vocab.txt'
+config_path = 'models/chinese_roformer-v2-char_L-6_H-384_A-6/bert_config.json'
+checkpoint_path = 'models/chinese_roformer-v2-char_L-6_H-384_A-6/bert_model.ckpt'
+dict_path = 'models/chinese_roformer-v2-char_L-6_H-384_A-6/vocab.txt'
 
 
 def corpus():
     """语料生成器
     """
     while True:
-        f = '/root/data_pretrain/data_shuf.json'
+        f = 'data/test.json'
         with open(f) as f:
             for l in f:
                 l = json.loads(l)
@@ -57,6 +57,7 @@ def text_process(text):
         result += text
     if result:
         yield result
+
 
 tokenizer = Tokenizer(
     dict_path,
@@ -89,6 +90,7 @@ def random_masking(token_ids):
 class data_generator(DataGenerator):
     """数据生成器
     """
+
     def __iter__(self, random=False):
         for is_end, text in self.sample(random):
             token_ids, segment_ids = tokenizer.encode(text, maxlen=maxlen)
@@ -99,6 +101,7 @@ class data_generator(DataGenerator):
 class CrossEntropy(Loss):
     """交叉熵作为loss，并mask掉输入部分
     """
+
     def compute_loss(self, inputs, mask=None):
         y_true, y_pred = inputs
         y_mask = K.cast(K.not_equal(y_true, 0), K.floatx())
@@ -150,6 +153,7 @@ with strategy.scope():
 class Evaluator(keras.callbacks.Callback):
     """训练回调
     """
+
     def on_epoch_end(self, epoch, logs=None):
         model.save_weights('bert_model.weights')  # 保存模型
 
